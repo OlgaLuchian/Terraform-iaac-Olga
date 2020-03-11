6cd0243bbde2b65542da408638e6bed0219baac0
Cloud User <centos@ip-172-31-23-122.eu-west-2.compute.internal>
2020-03-11 01:38:29 +0000

    added

--- VPC/subnet.tf
+++ VPC/subnet.tf
@@ -1,44 +1,53 @@
-#Public Subnets
-resource "aws_subnet" "dev1" { 
-  vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = "${var.cidr_block1_public}" 
-  }
+# public subnets
+
+resource "aws_subnet" "dev1" { 
+
+  vpc_id = "${aws_vpc.dev.id}" 
+
+  cidr_block = "${var.cidr_block1_public}" 
 
 
- 
-resource "aws_subnet" "dev2" { 
-  vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = "${var.cidr_block2_public}" 
-  
-  
-} 
+} 
 
-resource "aws_subnet" "dev3" { 
-  vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = "${var.cidr_block3_public}" 
-  
-  
-} 
+resource "aws_subnet" "dev2" { 
 
+  vpc_id = "${aws_vpc.dev.id}" 
 
-#Private Subnets 
-resource "aws_subnet" "dev_private1" { 
-  vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = "${var.cidr_block1_private}" 
-  }
+  cidr_block = "${var.cidr_block2_public}" 
+
+} 
+
+resource "aws_subnet" "dev3" { 
+  vpc_id = "${aws_vpc.dev.id}" 
+
+  cidr_block = "${var.cidr_block3_public}" 
 
 
- 
-resource "aws_subnet" "dev_private2" { 
-  vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = "${var.cidr_block2_private}" 
-  
-  
-} 
+} 
 
-resource "aws_subnet" "dev_private3" { 
-  vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = "${var.cidr_block3_private}" 
-  
-  
-} 
+# private subnets
+
+resource "aws_subnet" "dev_private1" { 
+
+  vpc_id = "${aws_vpc.dev.id}" 
+
+  cidr_block = "${var.cidr_block1_private}"
+
+
+} 
+
+resource "aws_subnet" "dev_private2" { 
+
+  vpc_id = "${aws_vpc.dev.id}" 
+
+  cidr_block = "${var.cidr_block2_private}"
+
+} 
+
+resource "aws_subnet" "dev_private3" { 
+  vpc_id = "${aws_vpc.dev.id}" 
+
+  cidr_block = "${var.cidr_block3_private}" 
+
+
+}
