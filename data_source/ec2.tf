6917f0566d111e8021983cff40df7f87f88f9152
Cloud User <centos@ip-172-31-27-167.eu-west-2.compute.internal>
2020-03-13 00:28:13 +0000

    added

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -19,6 +19,39 @@ data "aws_ami" "ubuntu" {
 }
 
 
+output "UBUNTU_AMI_ID" {
+  value       = "${data.aws_ami.ubuntu.id}"
+  
+  }
+
+#Gets CentOS AMI 
+  data "aws_ami" "centos" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["679593333241"] # Canonical
+}
+
+
 output "AMI_ID" {
-  value       = "${data.aws_ami.ubuntu.id"
+  value       = "${data.aws_ami.ubuntu.id}"
+  
+  }
+
+  resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
