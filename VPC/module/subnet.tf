01c887c3ba8be52cbddb2a6ef6717449cc1764a0
Cloud User <centos@ip-172-31-27-167.eu-west-2.compute.internal>
2020-03-14 20:39:14 +0000

    added

--- VPC/module/subnet.tf
+++ VPC/module/subnet.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "dev1" {
 
   cidr_block = "${var.cidr_block1_public}" 
 
-
+  availability_zone = "${var.az1}"
 } 
 
 resource "aws_subnet" "dev2" { 
@@ -14,14 +14,18 @@ resource "aws_subnet" "dev2" {
   vpc_id = "${aws_vpc.dev.id}" 
 
   cidr_block = "${var.cidr_block2_public}" 
-
+  
+  availability_zone = "${var.az2}"
 } 
 
+
 resource "aws_subnet" "dev3" { 
+  
   vpc_id = "${aws_vpc.dev.id}" 
 
   cidr_block = "${var.cidr_block3_public}" 
-
+  
+  availability_zone = "${var.az3}"
 
 } 
 
@@ -32,7 +36,7 @@ resource "aws_subnet" "dev_private1" {
   vpc_id = "${aws_vpc.dev.id}" 
 
   cidr_block = "${var.cidr_block1_private}"
-
+  availability_zone = "${var.az1}"
 
 } 
 
@@ -41,13 +45,14 @@ resource "aws_subnet" "dev_private2" {
   vpc_id = "${aws_vpc.dev.id}" 
 
   cidr_block = "${var.cidr_block2_private}"
-
+  availability_zone = "${var.az2}"
 } 
 
 resource "aws_subnet" "dev_private3" { 
+  
   vpc_id = "${aws_vpc.dev.id}" 
 
   cidr_block = "${var.cidr_block3_private}" 
-
+  availability_zone = "${var.az3}"
 
 }
